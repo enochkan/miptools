dc693f341684aec8727cafd739cda8465a48f627
Enoch Kan <kanxx030@gmail.com>
2019-11-06 20:27:03 -0600

    updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='miptools',
-      version='0.0.1',
+      version='0.0.2',
       description='miptools is a preprocessing utility that allows users to preprocess medical images at ease',
       url='https://github.com/chinokenochkan/miptools',
       author='Chi Nok Enoch Kan',
